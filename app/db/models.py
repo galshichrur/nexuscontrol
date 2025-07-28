db305704890302a5adb987923c907c28005f2295
Gal <82903124+maybegal@users.noreply.github.com>
2025-07-28 16:29:19 +0300

    Update agent connections table

--- app/db/models.py
+++ app/db/models.py
@@ -1,9 +1,14 @@
-from components import Field
-from table import Table
+from db.components import Field
+from db.table import Table
 
-# Agent table
-id_field = Field("id", int, primary=True)
-hostname = Field("hostname", str, unique=True)
-ip = Field("ip", str, unique=True)
+# Connections table
+agent_id = Field("agent_id", int, primary=True, unique=True)
+connection_time = Field("connection_time", str)  # A timestamp of when the agent connected.
+disconnect_time = Field("disconnect_time", str)  # A timestamp of when the agent disconnected.
+status = Field("status", bool)
 
-agents_table = Table("agents", (id_field, hostname, ip))
+hostname = Field("hostname", str)
+ip_address = Field("ip_address", str)
+port = Field("port", str)
+
+agents_table = Table("agents", (agent_id, connection_time, disconnect_time, status, hostname, ip_address, port))
