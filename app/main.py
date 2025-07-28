91322c0351c2065f9bbd615d6f540f44a3cadc11
Gal <82903124+maybegal@users.noreply.github.com>
2025-07-28 16:18:44 +0300

    Refactor main.py

--- app/main.py
+++ app/main.py
@@ -11,7 +11,7 @@ import os
 load_dotenv()
 
 engine: Engine = Engine()
-server: Server = Server()
+server: Server = Server(engine)
 
 app = FastAPI(title="Nexus Control", version="0.1.0")
 app.mount("/", StaticFiles(directory="../frontend/out", html=True), name="static")
@@ -25,15 +25,24 @@ async def on_startup():
     # Start server
     server.start()
 
-def init_db() -> None:
-    engine.open(os.getenv("DB_PATH"))
-    create = Create(agents_table, exists_ok=True)
-    engine.execute(create)
-    engine.commit()
-
 @app.on_event("shutdown")
 async def on_shutdown():
 
     # Close DB
     engine.commit()
     engine.close()
+
+def init_db() -> None:
+    engine.open(os.getenv("DB_PATH"))
+    create = Create(agents_table, exists_ok=True)
+    engine.execute(create)
+    engine.commit()
+
+@app.get("/agents")
+async def get_agents():
+    """Returns a list of connected agents."""
+    agents = [
+        {"id": agent_id, **agent["data"]}
+        for agent_id, agent in server.connected_clients.items()
+    ]
+    return {"agents": agents}
