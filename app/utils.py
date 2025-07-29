26fe0cac8a25d6ddf9733d7c98af9e99def32f71
Gal <82903124+maybegal@users.noreply.github.com>
2025-07-29 07:09:22 +0300

    Update utils.py to be static

--- app/utils.py
+++ app/utils.py
@@ -4,7 +4,6 @@ import requests
 import platform
 import datetime
 import time
-from typing import Generator
 from getmac import get_mac_address
 
 
@@ -20,24 +19,19 @@ os_architecture = platform.architecture()[0]
 server_time = str(datetime.datetime.now())
 
 
-def get_network_usage(interval: int = 10) -> Generator[tuple[float, float], None, None]:
+def get_network_usage(interval: int = 5) -> tuple[float, float]:
     last_bytes_sent = psutil.net_io_counters().bytes_sent
     last_bytes_recv = psutil.net_io_counters().bytes_recv
 
-    while True:
-        time.sleep(interval)
-        current_bytes_sent = psutil.net_io_counters().bytes_sent
-        current_bytes_recv = psutil.net_io_counters().bytes_recv
+    time.sleep(interval)
+    current_bytes_sent = psutil.net_io_counters().bytes_sent
+    current_bytes_recv = psutil.net_io_counters().bytes_recv
 
-        bytes_sent_diff = current_bytes_sent - last_bytes_sent
-        bytes_recv_diff = current_bytes_recv - last_bytes_recv
+    bytes_sent_diff = current_bytes_sent - last_bytes_sent
+    bytes_recv_diff = current_bytes_recv - last_bytes_recv
 
-        yield (bytes_recv_diff / interval), (bytes_sent_diff / interval)
-
-        last_bytes_sent = current_bytes_sent
-        last_bytes_recv = current_bytes_recv
+    return (bytes_recv_diff / interval), (bytes_sent_diff / interval)
 
 
-network_usage = get_network_usage()
-while True:
-    network_download_kbps, network_upload_kbps = next(network_usage)
+network_download_kbps, network_upload_kbps = get_network_usage()
+print(network_download_kbps, network_upload_kbps)
