3c468e5c1e0c2d30833d1c2f1436cb64ce42171f
Gal <82903124+maybegal@users.noreply.github.com>
2025-09-06 16:24:12 +0300

    Update application level communication protocol in app folder (server)

--- app/main.py
+++ app/main.py
@@ -185,7 +185,7 @@ async def get_agent(agent_id: str) -> AgentData:
 async def agent_interaction(agent_id: str, command: str) -> AgentResponse:
     try:
         response: dict = server.interact_with_agent(agent_id, command)
-        return AgentResponse(status=response["status"], command_response=response["command_response"], cwd=response["cwd"])
+        return AgentResponse(status=response["status"], command_response=response["response"], cwd=response["cwd"])
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
--- app/server.py
+++ app/server.py
@@ -27,7 +27,7 @@ class Server:
         self.MAX_CONNECTIONS: int = Config.MAX_CONNECTIONS
 
         self.connected_agents: dict[str, socket.socket] = {}
-        self.pending_command_responses: dict[str, dict] = {}
+        self.pending_agent_responses: dict[str, dict] = {}
 
     def start(self, host: str = "0.0.0.0", port: int = 8080) -> None:
 
@@ -102,31 +102,33 @@ class Server:
         try:
             client_socket.settimeout(self.HEARTBEAT_TIMEOUT)  # Set timeout.
 
-            initial_connection_info: dict = receive_json(client_socket)
+            agent_hello: dict = receive_json(client_socket)
+            if agent_hello.get("type") != "agent-hello":
+                raise Exception("Received invalid message type.")
 
-            agent_uuid = initial_connection_info.get("agent_id")
+            agent_uuid = agent_hello.get("agent_id")
             if agent_uuid is None:
                 agent_uuid = str(uuid.uuid4())
 
             current_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
             data = {
                 "agent_id": agent_uuid,
-                "name": initial_connection_info.get("hostname", "unknown"),
+                "name": agent_hello.get("hostname", "unknown"),
                 "connection_time": current_time,
                 "host": address[0],
                 "port": str(address[1]),
                 "status": True,
 
-                "hostname": initial_connection_info.get("hostname", "unknown"),
-                "cwd": initial_connection_info.get("cwd", "unknown"),
-                "os_name": initial_connection_info.get("os_name", "unknown"),
-                "os_version": initial_connection_info.get("os_version", "unknown"),
-                "os_architecture": initial_connection_info.get("os_architecture", "unknown"),
-                "local_ip": initial_connection_info.get("local_ip", "unknown"),
-                "public_ip": initial_connection_info.get("public_ip", "unknown"),
-                "mac_address": initial_connection_info.get("mac_address", "unknown"),
-                "is_admin": initial_connection_info.get("is_admin", "unknown"),
-                "username": initial_connection_info.get("username", "unknown")
+                "hostname": agent_hello.get("hostname", "unknown"),
+                "cwd": agent_hello.get("cwd", "unknown"),
+                "os_name": agent_hello.get("os_name", "unknown"),
+                "os_version": agent_hello.get("os_version", "unknown"),
+                "os_architecture": agent_hello.get("os_architecture", "unknown"),
+                "local_ip": agent_hello.get("local_ip", "unknown"),
+                "public_ip": agent_hello.get("public_ip", "unknown"),
+                "mac_address": agent_hello.get("mac_address", "unknown"),
+                "is_admin": agent_hello.get("is_admin", "unknown"),
+                "username": agent_hello.get("username", "unknown")
             }
 
             # Check if agent exists in the database
@@ -145,8 +147,12 @@ class Server:
                 db_engine_thread.execute(insert)
                 logger.info(f"New agent stored to agent table in db {address[0]}:{address[1]}, agent ID: {agent_uuid}.")
 
-            # Send back the agent_uid
-            send_json(client_socket, agent_uuid)
+            # Construct and send server hello message
+            server_hello = {
+                "type": "server-hello",
+                "agent_id": agent_uuid
+            }
+            send_json(client_socket, server_hello)
             db_engine_thread.commit()
 
             # Store agent socket
@@ -160,9 +166,9 @@ class Server:
                     if response is None:
                         break
 
-                    if response.get("type") == "command_response":
-                        self.pending_command_responses[response["command_id"]] = response
-                        logging.info(f"Received command response from agent {agent_uuid}.")
+                    if response.get("type") == "response":
+                        self.pending_agent_responses[response["response_id"]] = response
+                        logging.info(f"Received response from agent {agent_uuid}.")
 
                     elif response.get("type") == "heartbeat":  # Heartbeat from agent to keep the connection alive.
                         logging.info(f"Server heartbeat received from agent {agent_uuid}.")
@@ -192,47 +198,47 @@ class Server:
     def interact_with_agent(self, agent_uid: str, command: str) -> dict[str, bool | None] | None:
 
         try:
-            command_id = str(uuid.uuid4())
+            request_id = str(uuid.uuid4())
             agent_socket = self.connected_agents[agent_uid]
             if not agent_socket:
                 self._set_agent_offline(agent_uid)
                 return {
                     "status": False,
-                    "command_response": None,
+                    "response": None,
                     "cwd": None,
                 }
             data = {
-                "command_id": command_id,
+                "type": "request",
+                "request_id": request_id,
                 "command": command,
             }
-
-            send_json(agent_socket, data)  # Send cmd.
+            send_json(agent_socket, data)
 
             # Wait for the response to be received.
             elapsed = 0
-            while command_id not in self.pending_command_responses and elapsed < self.CMD_TIMEOUT:
+            while request_id not in self.pending_agent_responses and elapsed < self.CMD_TIMEOUT:
                 time.sleep(0.05)
                 elapsed += 0.05
 
             if elapsed >= self.CMD_TIMEOUT:
                 return {
                     "status": False,
-                    "command_response": "Error: command timed out.",
+                    "response": "Error: command timed out.",
                     "cwd": None,
                 }
 
-            response_data = self.pending_command_responses.pop(command_id)
+            response_data = self.pending_agent_responses.pop(request_id)
             if response_data:
                 return {
                     "status": True,
-                    "command_response": response_data.get("command_response"),
+                    "response": response_data.get("response"),
                     "cwd": response_data.get("cwd"),
                 }
 
             self._set_agent_offline(agent_uid)
             return {
                 "status": False,
-                "command_response": None,
+                "response": None,
                 "cwd": None,
             }
         except Exception as e:
