4c7509b417c2e8a033f936fb78272e60b42a2d4f
Gal <82903124+maybegal@users.noreply.github.com>
2025-10-03 13:13:25 +0300

    Add crypto, update helper modules in agent side

---
+++ agent/crypto.py
@@ -0,0 +1,54 @@
+import os
+import socket
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+
+
+class Crypto:
+
+    KEY_LENGTH = 32
+
+    @staticmethod
+    def encrypt(key: bytes, data: bytes) -> tuple[bytes, bytes]:
+        """
+        Encrypts data using AES-GCM. Returns ciphertext, nonce used to encrypt data.
+        """
+
+        aes = AESGCM(key)
+        nonce = os.urandom(12)
+        ct = aes.encrypt(nonce, data, None)
+        return ct, nonce
+
+    @staticmethod
+    def decrypt(key: bytes, data: bytes, nonce: bytes) -> bytes:
+        """
+        Decrypts data using AES-GCM. Returns plaintext.
+        """
+
+        aes = AESGCM(key)
+        pt = aes.decrypt(nonce, data, None)
+        return pt
+
+    @staticmethod
+    def send_secure(s: socket.socket, key: bytes, data: bytes) -> None:
+        """
+        Sends a secure message to the agent using shared key with AES-GCM.
+        """
+
+        ct, nonce = Crypto.encrypt(key, data)
+        packet = nonce + ct
+        msg_len = len(packet).to_bytes(4, "big")
+        s.sendall(msg_len + packet)
+
+    @staticmethod
+    def receive_secure(s: socket.socket, key: bytes) -> bytes:
+        """
+        Receives a secure message from agent using shared key with AES-GCM.
+        Returns received plaintext in bytes.
+        """
+
+        msg_len = int.from_bytes(s.recv(4), "big")
+        packet = s.recv(msg_len)
+        nonce = packet[:12]
+        ct = packet[12:]
+        pt = Crypto.decrypt(key, ct, nonce)
+        return pt
--- agent/helper.py
+++ agent/helper.py
@@ -1,33 +1,16 @@
 import socket
 import json
+from crypto import Crypto
 
-BUFFER = 2048
 
-
-def send_json(s: socket.socket, data: dict) -> None:
-    """Serializes, encodes, and sends through socket data with its length."""
+def send_secure_json(s: socket.socket, key: bytes, data: dict) -> None:
+    """Serializes, encodes, and sends dict as json securely."""
 
     json_data = json.dumps(data).encode()
-    msg_len = len(json_data)
-    s.send(msg_len.to_bytes(4, byteorder="big") + json_data)
+    Crypto.send_secure(s, key, json_data)
 
+def receive_secure_json(s: socket.socket, key: bytes) -> dict | None:
+    """Receives and decodes data to dict securely."""
 
-def receive_json(s: socket.socket) -> dict | None:
-    """Receives and decodes data from socket with its length."""
-
-    raw_msg_len = s.recv(4)
-    if not raw_msg_len:
-        return None
-    msg_len = int.from_bytes(raw_msg_len, byteorder="big")
-
-    chunks = []
-    received_bytes = 0
-    while received_bytes < msg_len:
-        chunk = s.recv(min(msg_len - received_bytes, BUFFER))
-        if not chunk:
-            return None
-        chunks.append(chunk)
-        received_bytes += len(chunk)
-
-    full_message = b"".join(chunks)
-    return json.loads(full_message.decode())
+    data = Crypto.receive_secure(s, key)
+    return json.loads(data.decode())
