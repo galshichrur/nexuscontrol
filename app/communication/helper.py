82a41e41c0f1ea449fceca1c74c1b42360176dfb
Gal <82903124+maybegal@users.noreply.github.com>
2025-10-03 12:58:50 +0300

    Remove unused buffer const

--- app/communication/helper.py
+++ app/communication/helper.py
@@ -3,9 +3,6 @@ import json
 from crypto import Crypto
 
 
-BUFFER = 2048
-
-
 def send_secure_json(s: socket.socket, key: bytes, data: dict) -> None:
     """Serializes, encodes, and sends dict as json securely."""
 
