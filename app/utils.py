71aa700edc834067232cef923a24496ed10e425c
Gal <82903124+maybegal@users.noreply.github.com>
2025-10-07 17:36:51 +0300

    Update utils new SystemStats class that dynamically updates usage stats

--- app/utils.py
+++ app/utils.py
@@ -4,38 +4,44 @@ import requests
 import platform
 import datetime
 import time
+import threading
+import state
 from getmac import get_mac_address
 
 
-def get_network_usage(interval: int = 1) -> tuple[float, float]:
-    last_bytes_sent = psutil.net_io_counters().bytes_sent
-    last_bytes_recv = psutil.net_io_counters().bytes_recv
+class SystemStats:
+    def __init__(self):
+        self.hostname = socket.gethostname()
+        self.local_ip = socket.gethostbyname(self.hostname)
+        self.public_ip = requests.get('https://api.ipify.org').text
+        self.mac_address = get_mac_address()
+        self.os_name = platform.system()
+        self.os_version = platform.version()
+        self.os_architecture = platform.architecture()[0]
+        self.server_start_time = str(datetime.datetime.now())
 
-    time.sleep(interval)
-    current_bytes_sent = psutil.net_io_counters().bytes_sent
-    current_bytes_recv = psutil.net_io_counters().bytes_recv
+        # Dynamic stats
+        self.cpu_usage = 0.0
+        self.memory_usage = 0.0
+        self.network_download_kbps = 0.0
+        self.network_upload_kbps = 0.0
+        self.server_time = str(datetime.datetime.now())
+        threading.Thread(target=self._background_update, daemon=True).start()
 
-    bytes_sent_diff = current_bytes_sent - last_bytes_sent
-    bytes_recv_diff = current_bytes_recv - last_bytes_recv
+    def _background_update(self):
+        while True:
+            self.cpu_usage = psutil.cpu_percent(interval=1)
+            self.memory_usage = psutil.virtual_memory().percent
 
-    return round((bytes_recv_diff / interval), 2), round((bytes_sent_diff / interval), 2)
+            net1 = psutil.net_io_counters()
+            time.sleep(5)
+            net2 = psutil.net_io_counters()
 
-hostname = socket.gethostname()
-local_ip = socket.gethostbyname(hostname)
-public_ip = requests.get('https://api.ipify.org').text
-mac_address = get_mac_address()
-cpu_usage = psutil.cpu_percent(1)
-memory_usage = psutil.virtual_memory().percent
-os_name = platform.system()
-os_version = platform.version()
-os_architecture = platform.architecture()[0]
-server_time = str(datetime.datetime.now())
-network_download_kbps, network_upload_kbps = get_network_usage()
-server_start_time = str(datetime.datetime.now())
+            recv_rate = (net2.bytes_recv - net1.bytes_recv) / 1024
+            sent_rate = (net2.bytes_sent - net1.bytes_sent) / 1024
 
-def update():
-    global cpu_usage, memory_usage, server_time, network_upload_kbps, network_download_kbps
-    cpu_usage = psutil.cpu_percent()
-    memory_usage = psutil.virtual_memory().percent
-    server_time = str(datetime.datetime.now())
-    network_download_kbps, network_upload_kbps = get_network_usage()
+            self.network_download_kbps = round(recv_rate, 2)
+            self.network_upload_kbps = round(sent_rate, 2)
+            self.server_time = str(datetime.datetime.now())
+
+system_stats = SystemStats()
